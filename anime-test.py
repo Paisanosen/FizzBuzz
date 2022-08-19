2dbb85e42dd1c3d2eb4f73d11b7e729fd65aa66e
Paisanosen <paisanosen@gmail.com>
2022-08-19 00:58:35 -0400

    Add English titles if they exist

--- anime-test.py
+++ anime-test.py
@@ -3,7 +3,6 @@ import json
 
     # problems:
     # cannot pass userinput variable into the query
-    # cannot iterate for loop of json response to read each anime title
     # anilistName = input("Enter your AniList username: ")
     # anilistType = "ANIME"
     # anilistStatus = "COMPLETED"
@@ -27,26 +26,19 @@ def main():
     }
     }
     """
-
     variables = {
         "userName": "Meltiply",
         "type": "ANIME",
         "status": "COMPLETED"
     }
     url = "https://graphql.anilist.co"
-    #-------------------------------------------------------------------------------------------#
-    # HTTP request to server
-    response = requests.post(url, json={"query": query, "variables": variables})
+    response = requests.post(url, json={"query": query, "variables": variables}) # HTTP request to server
     if response.status_code == 200:
-        # print(json.dumps(response.json(), indent=2))
         test_data_types(response)
-
-            # print(data["data"]["MediaListCollection"]["lists"])
     else:
         raise Exception(f"Error: {response.status_code}")
     #-------------------------------------------------------------------------------------------#
 
-        
 def test_data_types(response):
     if response.status_code == 200:
         userAnimeList = []
@@ -55,20 +47,19 @@ def test_data_types(response):
         print(type(animeListJson)) # str
         animeList = json.loads(animeListJson)
         print(type(animeList)) # dict
-        # print(animeList["data"]["MediaListCollection"]["lists"]) # trying to access the next key in the dictionary (entries) returns typeerror
         animeListData = animeList["data"]["MediaListCollection"]["lists"]
         print(type(animeListData))
         for data in animeListData:
-            # print(data)
-            print(type(data))
             entries = data["entries"]
-            # print(entries)
-            print(type(entries))
             for entry in entries:
-                # print(entry)
-                # print(type(entry))
-                animeTitle = entry["media"]["title"]["romaji"]
-                print(animeTitle)
+                if entry["media"]["title"]["english"] != None:
+                    animeTitle = entry["media"]["title"]["english"]
+                    userAnimeList.append(animeTitle)
+                else:
+                    animeTitle = entry["media"]["title"]["romaji"]
+                    userAnimeList.append(animeTitle)
+        for i in userAnimeList:
+            print(i)
     else:
         raise Exception(f"Error: {response.status_code}")
 
