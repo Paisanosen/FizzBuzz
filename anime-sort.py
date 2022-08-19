9c0e39bd35f1e1850838332296e024b0719a7f6e
Paisanosen <paisanosen@gmail.com>
2022-08-19 05:20:52 -0400

    Create bubble sort test function

---
+++ anime-sort.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+    animeList = ["Honey and Clover", "Hinamatsuri", "Ping Pong the Animation", "White Album 2"]
+    random.shuffle(animeList)
+
+    n = len(animeList)
+    print(n)
+    for i in range(n-1):
+        for j in range(0, (n - i - 1)):
+            print(animeList[j] + " or " + animeList[j + 1])
+            userChoice = input()
+            if userChoice == "L":
+                animeList[j], animeList[j + 1] = animeList[j + 1], animeList[j]
+            elif userChoice == "R":
+                pass
+    # show top anime at the top rather than bottom
+    animeList.reverse()
+    for i in range(len(animeList)):
+        print(animeList[i])
+
+if __name__ == "__main__":
+    main()
