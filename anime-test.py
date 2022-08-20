d4b19d5dd70e91240e22283e6b47c0059ce2ae3e
Paisanosen <paisanosen@gmail.com>
2022-08-19 23:18:16 -0400

    Update comments

--- anime-test.py
+++ anime-test.py
@@ -4,8 +4,14 @@ import random
 
     # to-do-list:
     # variables for type and status, not needed for now
-    # sort list alphabetically or by highest anilist rating?
     # if 404 error on username, return error and reprompt
+    # add error checking everywhere
+    # implement a different comparison method
+    # allow the user to skip an entry and come back to it later
+    # bubble method sort will pin the same anime against each other multiple times, want to avoid this
+    # divide parts of code into separate, distinct functions
+    # deploy as webapp with django
+    # save iterations by default sorting by anilist rating
 
 def main():
     #-------------------------------------------------------------------------------------------#
@@ -29,7 +35,7 @@ def main():
     variables = {
         "userName": anilistUsername,
         "type": "ANIME",
-        "status": "COMPLETED"
+        "status": "REPEATING"
     }
     url = "https://graphql.anilist.co"
     response = requests.post(url, json={"query": query, "variables": variables}) # HTTP request to server
@@ -78,9 +84,9 @@ def test_data_types(response):
             message = userAnimeList[j] + " or " + userAnimeList[j + 1] + "\n"
             # userChoice = input(message).upper()
             userChoice = random.randint(0, 1)
-            if userChoice == 0:
+            if userChoice == "L":
                 userAnimeList[j], userAnimeList[j + 1] = userAnimeList[j + 1], userAnimeList[j]
-            elif userChoice == 1:
+            elif userChoice == "R":
                 pass
     # show top anime at the top rather than bottom
     userAnimeList.reverse()
