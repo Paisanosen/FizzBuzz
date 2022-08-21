9cb82bb3d04e3c9369b748164f94c247061186d1
Paisanosen <paisanosen@gmail.com>
2022-08-21 12:34:25 -0400

    Add API fetch of average score

--- anime-test.py
+++ anime-test.py
@@ -12,24 +12,26 @@ import random
     # divide parts of code into separate, distinct functions
     # deploy as webapp with django
     # save iterations by default sorting by anilist rating
+    # sort initial array by average score
 
 def main():
     #-------------------------------------------------------------------------------------------#
     anilistUsername = input("Enter your AniList username: ")
     query = """
     query($userName: String) {
-    MediaListCollection(userName: $userName, type: ANIME, status: COMPLETED) {
-        lists {
-            entries {
-            media {
-                title {
-                romaji
-                english
+        MediaListCollection(userName: $userName, type: ANIME, status: COMPLETED) {
+            lists {
+                entries {
+                media {
+                    title {
+                    romaji
+                    english
+                        }
+                    averageScore
+                    }
                 }
             }
-            }
         }
-    }
     }
     """
     variables = {
@@ -47,22 +49,21 @@ def main():
 def test_data_types(response):
 
     userAnimeList = []
+    userAnimeListScores = []
 
     if response.status_code == 200:
 
-        animeListJson = json.dumps(response.json())
-        animeList = json.loads(animeListJson)
+        animeListJson = json.dumps(response.json()) # convert requests.models.Response to str
+        animeList = json.loads(animeListJson) # convert str to dict
         animeListData = animeList["data"]["MediaListCollection"]["lists"]
-        
-        # print(type(response)) # requests.models.Response
-        # print(type(animeListJson)) # str
-        # print(type(animeList)) # dict
-        # print(type(animeListData))
 
         for data in animeListData:
             entries = data["entries"]
             # if English exists, add the English title, else add the romaji title
             for entry in entries:
+                # append anime scores to different array
+                animeTitleScore = entry["media"]["averageScore"]
+                userAnimeListScores.append(animeTitleScore)
                 if entry["media"]["title"]["english"] != None:
                     animeTitle = entry["media"]["title"]["english"]
                     userAnimeList.append(animeTitle)
@@ -79,19 +80,20 @@ def test_data_types(response):
 
     n = len(userAnimeList)
     print(n)
-    for i in range(n-1):
-        for j in range(0, (n - i - 1)):
-            message = userAnimeList[j] + " or " + userAnimeList[j + 1] + "\n"
-            # userChoice = input(message).upper()
-            userChoice = random.randint(0, 1)
-            if userChoice == "L":
-                userAnimeList[j], userAnimeList[j + 1] = userAnimeList[j + 1], userAnimeList[j]
-            elif userChoice == "R":
-                pass
+    # for i in range(n-1):
+    #     for j in range(0, (n - i - 1)):
+    #         message = userAnimeList[j] + " or " + userAnimeList[j + 1] + "\n"
+    #         # userChoice = input(message).upper()
+    #         userChoice = random.randint(0, 1)
+    #         if userChoice == "L":
+    #             userAnimeList[j], userAnimeList[j + 1] = userAnimeList[j + 1], userAnimeList[j]
+    #         elif userChoice == "R":
+    #             pass
     # show top anime at the top rather than bottom
-    userAnimeList.reverse()
+    # userAnimeList.reverse()
     for i in range(len(userAnimeList)):
         print(userAnimeList[i])
+        print(userAnimeListScores[i])
     
 if __name__ == "__main__":
     main()
